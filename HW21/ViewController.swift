978a459d1ddb630eb42b28b80adc91ca47080987
MaksMalf <maksim.malofeev995@gmail.com>
2022-07-16 19:50:47 +0300

    Initial Commit

---
+++ HW21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW21
+//
+//  Created by Maksim Malofeev on 16/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
